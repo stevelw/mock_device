628b79cf02efa326e5dd44e1ad111240a42c311c
stevelw <steve@stevelw.co.uk>
2024-11-20 12:32:21 +0000

    rf: extract cat logic from device

--- src/cat.ts
+++ src/cat.ts
@@ -1,7 +1,6 @@
 import * as fs from "fs/promises";
 import { CatData } from "./types/CatTypes";
 
-
 export function catDataWithID(catID: number): Promise<CatData> {
   return fs
     .readFile(`${__dirname}/testData/cat_${catID}.json`, "utf-8")
@@ -9,4 +8,10 @@ export function catDataWithID(catID: number): Promise<CatData> {
       const catData: CatData = JSON.parse(rawData);
       return catData;
     });
+}
+
+export function catLocationAtIndex(catID: number, index: number) {
+  return catDataWithID(catID).then((catData) => {
+    return catData.trackingData[index].body;
+  });
 }
--- src/device.ts
+++ src/device.ts
@@ -1,15 +1,11 @@
 import axios from "axios";
 import "dotenv/config";
-import { CatDatapoint } from "./types/CatTypes";
-import { catDataWithID } from "./cat";
+import { DeviceUpdateBody } from "./types/DeviceTypes";
 
 const network = axios.create({
   baseURL: process.env.BACKEND_HOST,
 });
 
-function sendCatLocation(catID: number, index: number): Promise<void> {
-  return catDataWithID(catID).then((catData) => {
-    const catLocation: CatDatapoint = catData.trackingData[index];
-    network.post("/cats/location", catLocation.body);
-  });
+export function sendCatLocation(body: DeviceUpdateBody) {
+  return network.post("/cats/location", body);
 }
