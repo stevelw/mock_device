f0be6e3304aa17d4b28da450539dbc00f2d115d4
stevelw <steve@stevelw.co.uk>
2024-11-21 12:04:04 +0000

    include test data files in build

--- src/cat.ts
+++ src/cat.ts
@@ -3,7 +3,7 @@ import { CatData } from "./types/CatTypes";
 
 export function catDataWithID(catID: number): Promise<CatData> {
   return fs
-    .readFile(`${__dirname}/testData/cat_${catID}.json`, "utf-8")
+    .readFile(`./testData/cat_${catID}.json`, "utf-8")
     .then((rawData: any) => {
       const catData: CatData = JSON.parse(rawData);
       return catData;
